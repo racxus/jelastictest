a4af106f2daab22f00b14e1fa1edcd8f25024f85
root <root@lucid32.hsd1.ca.comcast.net>
2013-09-17 19:19:27 +0200

    added first stuff

---
+++ config/initializers/localeapp.rb
@@ -0,0 +1,5 @@
+require 'localeapp/rails'
+
+Localeapp.configure do |config|
+  config.api_key = 'PWcyFQwKF2yDvINxmSXHEWiN42PRHB070v4k3ZPhNQ8Jr11dbM'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = '1dc804bd12eac699e9a3f5811302a76b94c4cb0fd744f4f8aa8efb4919c1913d670f0de59c75a92fa87c1f4a68b5315ef88ac08c8648422e0f4f7de6a58ea2ab'
